557fac2597b40d70be32ba9318fbe7a47aa74f62
fiorella valencia <89022353+fiorella813valencia@users.noreply.github.com>
2024-09-22 01:23:20 -0500

    Add first commit

---
+++ Backend_Go2Climb_IoT/src/main/java/com/example/backend_go2climb_iot/Application/Agency/Resource/AgencyResource.java
@@ -0,0 +1,14 @@
+package com.example.backend_go2climb_iot.Application.Agency.Resource;
+
+public class AgencyResource {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private int phoneNumber;
+    private String description;
+    private String location;
+    private int ruc;
+    private String photo;
+    private int score;
+}
